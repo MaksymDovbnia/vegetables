b4424566318ad8f518b1e7868641e71f7132908d
YarikBasuk <basyuk.yaroslav@ukr.net>
2014-08-07 22:05:03 +0300

    fdbcvbcvb

---
+++ core/src/com/game/vegetables/screens/NEwClass.java
@@ -0,0 +1,11 @@
+package com.game.vegetables.screens;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Windows
+ * Date: 07.08.14
+ * Time: 22:04
+ * To change this template use File | Settings | File Templates.
+ */
+public class NEwClass {
+}
